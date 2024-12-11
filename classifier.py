9599bfae53369418567d8c9f835c5d073bb7af12
michalsciubisz <michalsci@student.agh.edu.pl>
2024-12-11 23:35:40 +0100

    initial data preprocessing, prepared skeleton for the all files

---
+++ classifier.py
@@ -0,0 +1,4 @@
+import tensorflow as tf
+import pandas as pd
+
+#TODO klasy algorytmów -> do użycia w GUI
---
+++ gui.py
@@ -0,0 +1,5 @@
+import tkinter as tk
+from algorithm import *
+from classifier import *
+
+#TODO implementacja GUI
