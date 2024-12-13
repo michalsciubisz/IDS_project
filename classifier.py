57226f285aeddc6a88edd306ca38e2e5a122a4e1
michalsciubisz <michalsci@student.agh.edu.pl>
2024-12-13 20:44:50 +0100

    chosen classifiers, data adjusted to fitting process

--- classifier.py
+++ classifier.py
@@ -1,4 +1,10 @@
 import tensorflow as tf
 import pandas as pd
 
-#TODO klasy algorytmów -> do użycia w GUI
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree  import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
